03b4bc35648601c250810c3cb2e9ebe03c027d58
macadphotos <shockingpants@hotmail.com>
2019-08-24 19:24:35 +0800

    First commit

---
+++ 10min.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+""" 10min pandas tutorial
+
+Introduction
+=============
+This tutorial introduces the user to **Pandas** usage. Additionally, the code is
+written in Markup language syntax so that we can generate documentation for the
+script with Sphinx automatically. There will also be a few examples to
+demonstrate how ipynb notebooks can be generated and uploaded. Lastly, the code
+will be uploaded to github to demonstrate the process of version control and
+file/documentation sharing. Links to the guides can be found in the
+:ref:`resource` section.
+
+
+.. _resource:
+Resources
+=========
+Links to online guides to materials dicussed in this tutorial can be found below
+`Pandas Guide<https://pandas.pydata.org/pandas-docs/stable/getting_started/10min.html>`_\
+`Sphinx Markup Guide<https://thomas-cokelaer.info/tutorials/sphinx/rest_syntax.html#text-formatting>`_
+
+"""
+print('Hello World')
